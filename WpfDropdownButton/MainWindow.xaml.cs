22217b594856cb0b51a12e0fd1020608af404034
spetrik <sergei.petrik@gmail.com>
2020-01-08 14:42:16 +0200

    refactored

--- WpfDropdownButton/MainWindow.xaml.cs
+++ WpfDropdownButton/MainWindow.xaml.cs
@@ -18,29 +18,28 @@ namespace WpfDropdownButton
     {
       if (sender is ToggleButton button && Equals(button.IsChecked, true))
       {
-        var cm = button.Resources["cm"] as ContextMenu;
-        if (cm.PlacementTarget == null)
+        var items = button.Resources["items"];
+        if (items is ContextMenu)
         {
-          cm.PlacementTarget = button;
-          cm.Placement = PlacementMode.Bottom;
-          cm.Closed += (senderClosed, eClosed) => ((ToggleButton)sender).IsChecked = false;
-        }
-        cm.IsOpen = true;
-      }
-    }
-
-    private void MyVersion2_OnChecked(object sender, RoutedEventArgs e)
-    {
-      if (sender is ToggleButton button && Equals(button.IsChecked, true))
-      {
-        var cm = button.Resources["cm"] as Popup;
-        if (cm.PlacementTarget == null)
+          var cm = (ContextMenu) items;
+          if (cm.PlacementTarget == null)
+          {
+            cm.PlacementTarget = button;
+            cm.Placement = PlacementMode.Bottom;
+            cm.Closed += (senderClosed, eClosed) => ((ToggleButton)sender).IsChecked = false;
+          }
+          cm.IsOpen = true;
+        } else if (items is Popup)
         {
-          cm.PlacementTarget = button;
-          cm.Placement = PlacementMode.Bottom;
-          cm.Closed += (senderClosed, eClosed) => ((ToggleButton)sender).IsChecked = false;
+          var popup = (Popup)items;
+          if (popup.PlacementTarget == null)
+          {
+            popup.PlacementTarget = button;
+            popup.Placement = PlacementMode.Bottom;
+            popup.Closed += (senderClosed, eClosed) => ((ToggleButton)sender).IsChecked = false;
+          }
+          popup.IsOpen = true;
         }
-        cm.IsOpen = true;
       }
     }
 
